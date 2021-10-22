1feb3055ff816c006fb05797a28ce6bc61a062b7
piotte <61425273+piotte@users.noreply.github.com>
2021-10-22 13:08:56 +0300

    Refactored to array, improved and extended tests

--- src/main.java
+++ src/main.java
@@ -1,28 +1,20 @@
 import my.game.logic.gameCore;
 
+import java.util.Arrays;
+
 class Main {
     public static void main(String[] args) {
 
-        int speedOfPlayer1 = 5;
-        int speedOfPlayer2 = 0;
-        int speedOfPlayer3 = 2;
+        int[] playersSpeed = { 5, 0, 2 };
 
         // Допишите здесь логику так, что будет подсчитано и выведено
         // количество игроков, которые выбывают.
         // Если свет зелёный, то проходят все игроки (0 выбывают).
         // Если свет красный, то выбывает каждый, чья скорость не 0.
 
-        int playersOutOfGame = 0;
-        if (gameCore.isPlayerOut(speedOfPlayer1)) {
-            playersOutOfGame++;
-        }
-        if (gameCore.isPlayerOut(speedOfPlayer2)) {
-            playersOutOfGame++;
-        }
-        if (gameCore.isPlayerOut(speedOfPlayer3)) {
-            playersOutOfGame++;
-        }
 
-        System.out.println("Количество выбывших игроков: " + playersOutOfGame);
+        System.out.println("Количество выбывших игроков: " + gameCore.numberOfPlayerOut(playersSpeed));
+        System.out.println("Выбыли игроки со скоростями: " + Arrays.toString(gameCore.playersOut(playersSpeed)));
+        System.out.println("Остались игроки со скоростями: " + Arrays.toString(gameCore.playersRemain(playersSpeed)));
     }
 }
